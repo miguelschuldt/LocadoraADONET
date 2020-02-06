e3ddfb248f1be05a1dfd99d7f2b9279b5379b264
miguelSchuldt <miguelizador@gmail.com>
2020-02-06 15:29:05 -0300

    mapping done

--- DataAccessLayer/LocadoraDbContext.cs
+++ DataAccessLayer/LocadoraDbContext.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,5 +21,15 @@ namespace DataAccessLayer
         public DbSet<Genero> Generos { get; set; }
         public DbSet<Locacao> Locacaos { get; set; }
         public DbSet<Funcionario> Funcionarios { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+            modelBuilder.Configurations.AddFromAssembly(Assembly.GetExecutingAssembly());
+            modelBuilder.Properties().Where(c => c.PropertyType == typeof(string)).Configure(c => c.IsRequired().IsUnicode(false));
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ DataAccessLayer/Mappings/LocacaoMapConfig.cs
@@ -0,0 +1,18 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Mappings
+{
+    class LocacaoMapConfig : EntityTypeConfiguration<Locacao>
+    {
+        public LocacaoMapConfig()
+        {
+            this.ToTable("LOCACOES");
+        }
+    }
+}
